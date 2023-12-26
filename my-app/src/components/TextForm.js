aaafe6888a999232234f15f8f3a105df79261e3e
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2023-12-26 14:25:19 +0530

    enabling dark mode code added styling object

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
 
     const handleUpClick = () => {
@@ -10,6 +10,13 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLowClick = () => {
+        setText(text.toLowerCase());
+    }
+    const handleDeleteTextClick = () =>{
+        setText("");
+    }
+
 
     const handleOnChange = (event) => {
         // console.log("On change");
@@ -17,12 +24,23 @@ export default function TextForm(props) {
     }
 
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea type="text" className="form-control" id="exampleFormCOntrolTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button type="button" className="btn btn-danger" onClick={handleDeleteTextClick}>Delete Text</button>
         </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <b><p>{text.split(" ").length} words, {text.length} charaacters</p></b>
+            <p>{0.008 * text.split(" ").length} minutes to read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
